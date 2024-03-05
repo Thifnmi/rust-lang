9850b37f1d33a6f9e2b892814dd2c2aec361a7f7
Thifnmi <tuthin2k@gmail.com>
2024-03-05 13:34:03 +0700

    docs and example enums

--- enum.rs
+++ enum.rs
@@ -142,3 +142,14 @@ fn match_is_expression() {
         _ => '*',
     });
 }
+
+fn guard_match_construct() {
+    for n in -2..5 {
+        println!("{} is {}.", n, match n {
+            0 => "zero",
+            1 => "one",
+            _ if n < 0 => "negative",
+            _ => "plural",
+        });
+    }
+}
